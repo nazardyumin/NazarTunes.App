f92b0434d5b92ad297810b744cdfd9835e526894
Nazar <Dumi_sj18@itstepru.onmicrosoft.com>
2022-11-12 18:31:10 +0500

    Updates!

---
+++ Views/NazarTunesWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NazarTunes.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для NazarTunesWindow.xaml
+    /// </summary>
+    public partial class NazarTunesWindow : Window
+    {
+        public NazarTunesWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
